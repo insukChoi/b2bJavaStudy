3bfba18bf74bfb3a7599917d7f129deed3b88e46
leeeeel <aanalogg@naver.com>
2017-03-27 18:07:03 +0900

    create PACKAGE

---
+++ src/com/b2b/junsang/test.java
@@ -0,0 +1,10 @@
+package com.b2b.insuk.ch2;
+
+/**
+ * Created by admin on 2017-03-27.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.print("create junsang package");
+    }
+}
